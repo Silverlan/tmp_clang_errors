300e17b035c4fe066b95b5d14e3799b71cdcfc2d
Silverlan <silverlan@pragma-engine.com>
2025-12-29 20:27:59 +0100

    f

--- lib_b/main.cpp
+++ lib_b/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+import test_module;
+
 int main() {
+    std::cout<<"Vec: "<<test::A.x<<","<<test::A.y<<","<<test::A.z<<std::endl;
     std::cout << "Hello World from Clang " << __clang_version__ << "!" << std::endl;
     return 0;
 }
