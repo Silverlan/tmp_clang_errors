1ab40372c08ccc86ebe054366fad1b4380fc9912
Silverlan <silverlan@pragma-engine.com>
2025-12-30 21:38:33 +0100

    ci: update workflows

--- test_program/build.bat
+++ test_program/build.bat
@@ -7,3 +7,5 @@ clang++.exe -shared -o test_module\test_module.dll test_module\test_module.o -Wl
 clang++.exe -std=c++20 main.cpp -fprebuilt-module-path=test_module -c -o main.o -DTEST_CASE=%1
 clang++.exe main.o test_module\test_module.lib -o app.exe
 .\app.exe
+
+endlocal
