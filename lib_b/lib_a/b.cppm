cf216a92e850a18e6dfe1d6acd5a569c6ec19ad4
Silverlan <silverlan@pragma-engine.com>
2025-12-30 12:12:41 +0100

    f

--- lib_b/lib_a/b.cppm
+++ lib_b/lib_a/b.cppm
@@ -3,13 +3,9 @@ export module test_module;
 
 export import glm;
 
-struct Vector3 {
-    float x;
-    float y;
-    float z;
-};
-
 export namespace test {
-    constexpr Vector3 A = Vector3 {1.f,2.f,3.f};
-    constexpr glm::vec3 B = glm::vec3{1.f,2.f,3.f};
+    constexpr glm::vec3 test_vector = glm::vec3{1.f,2.f,3.f};
+    __declspec(dllexport) void test_function(const glm::vec3 &v)
+    {
+    }
 }
--- lib_b/main.cpp
+++ lib_b/main.cpp
@@ -2,9 +2,34 @@
 
 import test_module;
 
+#if TEST_CASE == 4
+#pragma clang optimize off
+#endif
+
+#if TEST_CASE == 3 || TEST_CASE == 4
+static void print_vec(const glm::vec3 v)
+{
+    std::cout<<v.x<<","<<v.y<<","<<v.z<<std::endl;
+}
+#endif
+
 int main() {
-    std::cout<<"VecA: "<<test::A.x<<","<<test::A.y<<","<<test::A.z<<std::endl;
-    std::cout<<"VecB: "<<test::B.x<<","<<test::B.y<<","<<test::B.z<<std::endl;
-    std::cout << "Hello World from Clang " << __clang_version__ << "!" << std::endl;
+#if TEST_CASE == 0
+    // Build successful
+    std::cout<<test::test_vector.x<<std::endl;
+#elif TEST_CASE == 1
+    // lld-link: error: undefined symbol: struct glm::vec<3, float, 0> const test::test_vector
+    std::cout<<test::test_vector<<std::endl;
+#elif TEST_CASE == 2
+    // lld-link: error: undefined symbol: struct glm::vec<3, float, 0> const test::test_vector
+    test::test_function(test::test_vector);
+#else
+    // TEST_CASE == 3: Build successful
+    // TEST_CASE == 4: lld-link: error: undefined symbol: struct glm::vec<3, float, 0> const test::test_vector
+    print_vec(test::test_vector);
+#endif
     return 0;
 }
+#if TEST_CASE == 4
+#pragma clang optimize on
+#endif
