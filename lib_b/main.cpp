db1e440ddba82a04cd16b6178e9ba9c04360e68a
Silverlan <silverlan@pragma-engine.com>
2025-12-30 12:57:25 +0100

    f

--- lib_b/main.cpp
+++ lib_b/main.cpp
@@ -2,11 +2,11 @@
 
 import test_module;
 
-#if TEST_CASE == 4
+#if TEST_CASE == 3
 #pragma clang optimize off
 #endif
 
-#if TEST_CASE == 3 || TEST_CASE == 4
+#if TEST_CASE == 2 || TEST_CASE == 3
 static void print_vec(const glm::vec3 v)
 {
     std::cout<<v.x<<","<<v.y<<","<<v.z<<std::endl;
@@ -19,17 +19,14 @@ int main() {
     std::cout<<test::test_vector.x<<std::endl;
 #elif TEST_CASE == 1
     // lld-link: error: undefined symbol: struct glm::vec<3, float, 0> const test::test_vector
-    std::cout<<test::test_vector<<std::endl;
-#elif TEST_CASE == 2
-    // lld-link: error: undefined symbol: struct glm::vec<3, float, 0> const test::test_vector
     test::test_function(test::test_vector);
 #else
-    // TEST_CASE == 3: Build successful
-    // TEST_CASE == 4: lld-link: error: undefined symbol: struct glm::vec<3, float, 0> const test::test_vector
+    // TEST_CASE == 2: Build successful
+    // TEST_CASE == 3: lld-link: error: undefined symbol: struct glm::vec<3, float, 0> const test::test_vector
     print_vec(test::test_vector);
 #endif
     return 0;
 }
-#if TEST_CASE == 4
+#if TEST_CASE == 3
 #pragma clang optimize on
 #endif
