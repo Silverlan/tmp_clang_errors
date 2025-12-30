2d79b084f3a5aafce96b03db2675605ca6fa353f
Silverlan <silverlan@pragma-engine.com>
2025-12-30 21:58:12 +0100

    ci: update workflows

--- test_program/build.bat
+++ test_program/build.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 
-clang++.exe -std=c++20 test_module\test_module.cppm --precompile -o test_module\test_module.pcm
-clang++.exe -std=c++20 test_module\test_module.pcm -c -o test_module\test_module.o
-clang++.exe -fuse-ld=lld -shared -o test_module\test_module.dll test_module\test_module.o -Wl,/IMPLIB:test_module\test_module.lib
+clang++.exe -std=c++20 test_module/test_module.cppm --precompile -o test_module/test_module.pcm
+clang++.exe -std=c++20 test_module/test_module.pcm -c -o test_module/test_module.o
+clang++.exe -fuse-ld=lld -shared -o test_module/test_module.dll test_module/test_module.o -Wl,/IMPLIB:test_module/test_module.lib
 clang++.exe -std=c++20 main.cpp -fprebuilt-module-path=test_module -c -o main.o -DTEST_CASE=%1
-clang++.exe -fuse-ld=lld main.o test_module\test_module.lib -o app.exe
+clang++.exe -fuse-ld=lld main.o test_module/test_module.lib -o app.exe
 
 endlocal
